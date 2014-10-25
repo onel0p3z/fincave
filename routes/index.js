4247585c639048ffd9ec41cf0ca847c5bef3e3be
Cristina Solana <cs@nightshiftcreative.com>
2014-10-25 16:52:17 -0400

    use templates in routes

--- routes/index.js
+++ routes/index.js
@@ -8,12 +8,12 @@
     // frontend routes
     //var get_about_us = require('./frontend/get_about_us');
     var get_home = require('./frontend/get_home');
-    //var get_events = require('./frontend/get_events');
+    var get_event = require('./frontend/get_event');
 
     module.exports = [
         get_home,
         //get_about_us,
-        //get_events,
+        get_event,
         get_instagram,
         post_instagram
     ];
